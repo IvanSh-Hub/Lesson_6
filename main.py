5cc7e8c097138e0ec9316efb873d72c423abb0d4
IvanSh-Hub <81467385+IvanSh-Hub@users.noreply.github.com>
2023-02-19 13:09:02 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+# Задача 30: Заполните массив элементами арифметической прогрессии.
+# Её первый элемент, разность и количество элементов нужно ввести с клавиатуры.
+# Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+#
+#
+# firstElement = int(input("Enter the first element: "))
+# diff = int(input("Enter the difference: "))
+# numberOfElements = int(input("Enter the number of elements: "))
+#
+# def fun(n):
+#     return firstElement + (n - 1) * diff
+#
+# arr = list(map(lambda x: fun(x), range(numberOfElements)))
+#
+# print(arr)
+
+#=============================================================================================================
+#
+# Задача 32: Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+#
+# minElem = int(input("Enter min element of list: "))
+# maxElem = int(input("Enter max element of list: "))
+#
+# array = [x for x in range(minElem,maxElem)]
+# print(array)
+#
+# for i, elem in enumerate(array):
+#     print(i, end = " ")
+
+
+
+
+
+
+
+
